26db1a75afc6fa538987bb5df996e8e7a9ec0e0c
Aashish Jha <saiaashishj@gmail.com>
2026-06-24 13:33:53 +0530

    Complete Mission 003: Decision Making

---
+++ Semester-1/C Programming/decision.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int marks;
+    printf("Enter your marks: ");
+    scanf("%d",&marks);
+
+    if(marks>= 40)
+    {
+        printf("PASS\n");
+    }
+    else
+    {
+        printf("FAIL\n");
+    }
+
+    return 0;
+}
